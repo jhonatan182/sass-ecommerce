880a2a627c7c2457a3e8027d57da13bf11dd475c
Jhonatan Vargas <jvargas@sefin.gob.hn>
2026-05-06 10:05:53 -0600

    feat: pantalla de categorias

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,17 +1,40 @@
 import { notFound } from "next/navigation";
 
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import { Category } from "@/interfaces";
+
 type CategoryPageProps = {
   params: Promise<{
-    id: string;
+    id: Category;
   }>;
 };
+
+const seedProducts = initialData.products;
 
 export default async function CategoryPage({ params }: CategoryPageProps) {
   const { id } = await params;
 
-  if (id === "kids") {
-    notFound();
-  }
+  const products = seedProducts.filter((product) => product.gender === id);
 
-  return <div>Category {id}</div>;
+  const label: Record<Category, string> = {
+    men: "para hombres",
+    women: "para mujeres",
+    kid: "para niños",
+    unisex: "para todos",
+  };
+  // if (id === "kids") {
+  //   notFound();
+  // }
+
+  return (
+    <>
+      <Title
+        title={`Artículos ${label[id]}`}
+        subtitle="Todos los productos"
+        className="mb-2"
+      />
+      <ProductGrid products={products} />
+    </>
+  );
 }
