9ff5f0757c018840d6d32608df26df41352dc682
Jhonatan Vargas <jvargas@sefin.gob.hn>
2026-05-06 10:20:01 -0600

    feat: pantalla de producto

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,3 +1,52 @@
-export default function ProductSlugPage() {
-  return <div>Product Slug</div>;
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+type ProductSlugPageProps = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+const seedProducts = initialData.products;
+
+export default async function ProductSlugPage({
+  params,
+}: ProductSlugPageProps) {
+  const { slug } = await params;
+
+  const product = seedProducts.find((product) => product.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
+
+  return (
+    <div className="mt-5 mb-20 grid gap-3 md:grid-cols-3">
+      {/* Slideshow */}
+      <div className="col-span-1 md:col-span-2">hola</div>
+
+      {/* Detalles */}
+      <div className="col-span-1 px-5 ">
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">L.{product.price}</p>
+
+        {/* Selector de tallas */}
+
+        {/* Selector de cantidad */}
+
+        {/* Button */}
+        <button className="btn-primary my-5 cursor-pointer">
+          Agregar al carrito
+        </button>
+
+        {/* Descripción */}
+
+        <h3 className="font-bold text-sm">Descripción</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
+    </div>
+  );
 }
