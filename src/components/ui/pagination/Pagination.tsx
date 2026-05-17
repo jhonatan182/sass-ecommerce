9845f883e73623fa5fe0f027321c18ec6e00ab2b
jhonatan182 <jona.vand98@gmail.com>
2026-05-16 19:18:07 -0600

    feat: funcionamiento base de paginacion

--- src/components/ui/pagination/Pagination.tsx
+++ src/components/ui/pagination/Pagination.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
 
 import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
 
@@ -8,14 +11,40 @@ type Props = {
 };
 
 export function Pagination({ currentPage, totalPages }: Props) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPageFromUrl = Number(searchParams.get("page")) || 1;
+
+  const createPageUrl = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (pageNumber === "...") {
+      return `${pathname}?${params.toString()}`;
+    }
+
+    if (Number(pageNumber) <= 0) {
+      return `${pathname}`;
+    }
+
+    if (Number(pageNumber) > totalPages) {
+      return `${pathname}?${params.toString()}`;
+    }
+
+    params.set("page", pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
+
   return (
     <div className="flex text-center justify-center mt-10 mb-32">
       <nav aria-label="Page navigation">
         <ul className="flex list-style-none">
-          <li className="page-item">
+          <li
+            className={`page-item ${currentPage <= 1 ? "pointer-events-none opacity-50" : ""}`}
+          >
             <Link
               className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-              href="#"
+              href={createPageUrl(currentPage - 1)}
+              aria-disabled={currentPage <= 1}
             >
               <IoChevronBackOutline size={30} />
             </Link>
@@ -44,10 +73,12 @@ export function Pagination({ currentPage, totalPages }: Props) {
               3
             </a>
           </li>
-          <li className="page-item">
+          <li
+            className={`page-item  ${currentPage >= totalPages ? "pointer-events-none opacity-50" : ""}`}
+          >
             <Link
               className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-              href="#"
+              href={createPageUrl(currentPage + 1)}
             >
               <IoChevronForwardOutline size={30} />
             </Link>
