a6a57efe0c4d87fff10ba5c5e6d5b60fa574151b
jhonatan182 <jona.vand98@gmail.com>
2026-05-16 18:44:58 -0600

    chore: paginacion, total paginas, validacion cuando no hay productos

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 1. Clonar el repositorio
 2. Crear una copia del archivo `.env.example` y renombrarlo a `.env`
 3. Configurar las variables de entorno necesarias
-4. Instalar las dependencias con `bun install `
+4. Instalar las dependencias con `bun install`
 5. Ejecutar las migraciones de Prisma `bunx prisma migrete dev`
 6. Ejecutar el seed de datos `bun run seed`
 7. Ejecutar `bun run dev`
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,25 @@
+import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
+import { redirect } from "next/navigation";
 
-const products = initialData.products;
+type Props = {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+};
 
-export default function Home() {
+export default async function Home({ searchParams }: Props) {
+  const { page } = await searchParams;
+
+  const pageParams = page ? parseInt(page) : 1;
+
+  const { products, currentPage, totalPages } =
+    await getPaginatedProductsWithImages({
+      page: pageParams,
+    });
+
+  if (products.length === 0) {
+    redirect("/");
+  }
+
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
